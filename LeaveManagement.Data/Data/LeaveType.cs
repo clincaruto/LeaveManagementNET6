558bbd494f3ea91b7af19722e88e38875a5843f0
Clinton Clinton <clindrop@gmail.com>
2022-08-24 17:04:10 +0100

    Added Database Tables and Migrations, altered the dbset to employee

---
+++ LeaveManagement.Data/Data/LeaveType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Data.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        
+
+    }
+}
